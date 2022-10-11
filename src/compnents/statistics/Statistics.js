375047b02bb353f516453053a2401d1e058dff53
rahiislam96 <nazrulislam66188@gmail.com>
2022-10-11 16:24:26 +0600

    statistics component added

--- src/compnents/statistics/Statistics.js
+++ src/compnents/statistics/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const Statistics = () => {
+    const charts = useLoaderData();
     return (
-        <div className=' text-center mt-8'>
-            <h1>This is statistics zone</h1>
+        <div className='mt-8'>
+            <LineChart
+                width={500}
+                height={300}
+                data={charts.data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis dataKey="total" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };
