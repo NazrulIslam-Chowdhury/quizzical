67a917bd027eaee0b81318ebe879e861283640ce
rahiislam96 <nazrulislam66188@gmail.com>
2022-10-12 10:26:36 +0600

    header updated

--- src/compnents/header/Header.js
+++ src/compnents/header/Header.js
@@ -6,19 +6,21 @@ const Header = () => {
     const [open, setOpen] = useState(false);
 
     let activeStyle = {
-        textDecoration: "underline red  3px",
+        backgroundColor: 'white',
+        padding: '2px',
+        borderRadius: '5px'
     }
 
     return (
-        <div className="bg-red-300 w-full flex  justify-between p-3">
+        <div className="bg-red-300  flex  justify-between p-3">
 
-            <div onClick={() => setOpen(!open)} className="h-8 w-8 bg-red-300 text-black font-bolder md:hidden">
+            <div onClick={() => setOpen(!open)} className="h-8 w-8 text-black font-bolder md:hidden">
                 {
                     open ? <XMarkIcon /> : <Bars3Icon />
                 }
             </div>
 
-            <div className={`bg-red-300  w-full space-x-5 font-semibold absolute md:static duration-500 ease-in ml-8 ${open ? 'top-6 mt-4' : 'top-[-120px]'}`}>
+            <div className={`bg-red-300 w-full space-x-5 font-semibold absolute md:static duration-500 ease-in ml-8 ${open ? 'top-6 mt-4' : 'top-[-120px]'}`}>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to='/topics'>Your Topics</NavLink>
                 <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to='/statistics'>Statistics</NavLink>
--- src/compnents/statistics/Statistics.js
+++ src/compnents/statistics/Statistics.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip, Legend } from 'recharts';
 
 const Statistics = () => {
     const charts = useLoaderData();
     return (
-        <div className='w-full h-full mt-8'>
+        <div className='text-center mt-8'>
             <h1 className='ml-40 font-medium mb-5'>name x total question chart</h1>
-            <LineChart
-                width={500}
-                height={300}
-                data={charts.data}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis dataKey="total" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-            </LineChart>
+            <ResponsiveContainer height={400}>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={charts.data}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis dataKey="total" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
 
         </div>
     );
